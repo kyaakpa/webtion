094a86cef61c69e6680f56090a778a05f2be7a9b
kyaakpa <100368545+kyaakpa@users.noreply.github.com>
2023-11-14 01:35:41 +1100

    ...

--- app/contact/page.js
+++ app/contact/page.js
@@ -21,7 +21,7 @@ const Contact = () => {
     setIsLoading(true);
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/contact", {
+      const response = await fetch("https://webtion.vercel.app/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
